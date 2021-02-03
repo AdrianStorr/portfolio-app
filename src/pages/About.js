fd384d31bf87ef1587100691d9a32a630e4cb29e
Adrian Storr <Adrianloves123@gmail.com>
2021-02-03 12:32:09 -0500

    added components and pages

---
+++ src/pages/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const mainPage = () => {
+    return (
+        <div className="aboutApp">
+
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>
+                            About Me
+                </h3>
+                    </div>
+                    <div className="card-body">
+                        <blockquote className="blockquote mb-0">
+                            <div className="row">
+                                <div className="col-md-3">
+                                    <img id="pic" src="https://avatars0.githubusercontent.com/u/56319950?s=460&u=29ea5c9829ccb251f4c3e739338f1ba870b5ce7d&v=4" alt="profile pic" width="100%" height="auto%" />
+                                </div>
+
+                                <div className="col-md-8">
+
+                                <p>My name is Adrian Storr. I am a junior web developer currently registered with John Hopkins Coding Bootcamp, which I will complete in February 2021.</p>
+    
+    <p>My background is the completion of university and high school. I have a Bachelor's degree in project management, along with a professional certification in 
+        supply chain project management from Georgia Tech University.
+         I was also a co-owner of a clothing boutique responsible for transforming and empty store space, promotion, inventory, vendor selection and distributing salaries.
+         </p>
+        
+    <p>My next work is hopefully a full stack developer. I hope to gain enough knowledge to work in this sphere and gain even more experience.</p>
+<p>During my free time I like to read, develope projects and play golf.</p>
+</div>
+    </div>
+       </blockquote>
+    </div>
+</div>
+</div>
+
+        </div>
+    )
+}
+export default mainPage                
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,123 @@
+import React from "react";
+import "../App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const portfolio = () => {
+    return (
+        <div className="portfolioApp">
+            <div className="container">
+                <div className="card">
+                    <div className="card-header">
+                        <h3>
+                            Portfolio
+                    </h3>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-1">
+                            </div>
+
+                            <div className="col-md-3">
+                                <a href="https://adrianstorr.github.io/Weather-Dashboard-app/">
+                                    <img src="assets/img/weather1.png"
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">Weather Dashboard</p>
+                                <p id="repo"><a href="https://github.com/AdrianStorr/Weather-Dashboard-app">https://github.com/AdrianStorr/Weather-Dashboard-app</a></p>
+                            </div>
+                            <div className="col-md-1"></div>
+
+                            <div className="col-md-3">
+                                <a href="https://drive.google.com/file/d/1e93bwgnMhm7jqi-4lN2EWITR14xr717C/view?usp=sharing">
+                                    <img src="assets/img/work.day.png"
+                                        className="card-img-top" alt="img-two" />
+                                </a>
+                                <p className="card-text">Employee Generator</p>
+                                <p id="repo"><a href="https://github.com/AdrianStorr/Employee-generator"
+                                >https://github.com/AdrianStorr/Employee-generator</a></p>
+                            </div>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                        <div className="row">
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-3">
+                                <a href="https://adrianstorr.github.io/code-quiz/">
+                                    <img src="img/quiz3.png"
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">Code Quiz</p>
+                                <p id="repo"><a href="https://github.com/AdrianStorr/code-quiz"
+                                >https://github.com/AdrianStorr/code-quiz</a></p>
+                            </div>
+                            <div className="col-md-1">
+
+                            </div>
+                            <div className="col-md-3">
+                                <a href="https://chernanma.github.io/Project_1">
+                                    <img src="img/screen1.jpg"
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">Project-1</p>
+                                <p id="repo"><a href="https://chernanma.github.io/Project_1/"
+                                >https://github.com/chernanma/Project_1</a></p>
+                                <div className="col-md-1"></div>
+                            </div>
+                        </div>
+                        <br></br>
+                        <br></br>
+                        <div className="row">
+                            <div className="col-md-1"></div>
+                            <div className="col-md-3">
+                                <a href="https://peaceful-beyond-40794.herokuapp.com/">
+                                    <img src="assets/img/screenshot pasw.PNG"
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">Budget Tracker</p>
+                                <p id="repo"><a href="https://github.com/AdrianStorr/Pwahomework"
+                                >https://github.com/AdrianStorr/Pwahomework</a></p>
+
+                            </div>
+
+                            <div className="col-md-1"></div>
+
+                            <div className="col-md-3">
+                                <a href="https://vast-woodland-01889.herokuapp.com/">
+                                    <img src="assets/img/let's.travel.png"
+                                        className="card-img-top" alt="img-one" />
+                                </a>
+                                <p className="card-text">Project 2: Bike Shop App</p>
+                                <p id="repo"><a href="https://github.com/chernanma/BikeShopApp"
+                                >https://github.com/chernanma/BikeShopApp</a></p>
+
+                            </div>
+                        </div>
+                        <br></br>
+                        <div className="gitProfile">
+                            <p>GitHub Profile: <a href="https://github.com/AdrianStorr">
+                            https://github.com/AdrianStorr</a> </p>
+                            <p>Linkedin Profile: <a href="https://www.linkedin.com/in/adrian-storr-98773731"
+                            >https://www.linkedin.com/in/adrian-storr-98773731</a></p>
+                            <p>Email Address: Adrianloves123@gmail.com</p>
+                            <p>Phone Number:(850)-890-5740</p>
+                            <a href="./resumeUpdate.pdf"
+                            > Resume</a>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
+
+            </div>
+        </div>
+    )
+}
+export default portfolio
